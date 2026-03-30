d3d8108533143d498ea6f83900d1b7593864a5dc
Muhammad Liaquat <muhammadliaquatroy@gmail.com>
2026-03-30 22:41:28 +0300

    Define relational schema and table constraints

---
+++ scripts/01_db_schema_setup.sql
@@ -0,0 +1,7 @@
+/* ==============================================================================
+PROJECT: Brazilian E-Commerce Analysis (Olist)
+DESCRIPTION: Database Schema Setup and Table Creation
+AUTHOR: Muhammad Liaquat
+DATE: 2026-03-22
+==============================================================================
+*/
