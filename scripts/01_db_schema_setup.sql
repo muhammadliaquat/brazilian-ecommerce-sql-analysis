f4b8294c15d79aa85eb8a9f55dcd9c2cabf64670
Muhammad Liaquat <muhammadliaquatroy@gmail.com>
2026-03-30 22:42:37 +0300

    Update 01_db_schema_setup.sql

--- scripts/01_db_schema_setup.sql
+++ scripts/01_db_schema_setup.sql
@@ -5,3 +5,83 @@ AUTHOR: Muhammad Liaquat
 DATE: 2026-03-22
 ==============================================================================
 */
+CREATE TABLE customers (
+    customer_id VARCHAR(50) PRIMARY KEY,
+    customer_unique_id VARCHAR(50),
+    customer_zip_code_prefix INT,
+    customer_city VARCHAR(100),
+    customer_state CHAR(2)
+);
+
+CREATE TABLE sellers (
+    seller_id VARCHAR(50) PRIMARY KEY,
+    seller_zip_code_prefix INT,
+    seller_city VARCHAR(100),
+    seller_state CHAR(2)
+);
+
+CREATE TABLE products (
+    product_id VARCHAR(50) PRIMARY KEY,
+    product_category_name VARCHAR(100),
+    product_name_length INT,
+    product_description_length INT,
+    product_photos_qty INT,
+    product_weight_g INT,
+    product_length_cm INT,
+    product_height_cm INT,
+    product_width_cm INT
+);
+
+CREATE TABLE product_category_name_translation (
+    product_category_name VARCHAR(100),
+    product_category_name_english VARCHAR(100)
+);
+
+CREATE TABLE geolocation (
+    geolocation_zip_code_prefix INT,
+    geolocation_lat NUMERIC,
+    geolocation_lng NUMERIC,
+    geolocation_city VARCHAR(100),
+    geolocation_state CHAR(2)
+);
+
+
+CREATE TABLE orders (
+    order_id VARCHAR(50) PRIMARY KEY,
+    customer_id VARCHAR(50) REFERENCES customers(customer_id),
+    order_status VARCHAR(20),
+    order_purchase_timestamp TIMESTAMP,
+    order_approved_at TIMESTAMP,
+    order_delivered_carrier_date TIMESTAMP,
+    order_delivered_customer_date TIMESTAMP,
+    order_estimated_delivery_date TIMESTAMP
+);
+
+CREATE TABLE order_items (
+    order_id VARCHAR(50) REFERENCES orders(order_id),
+    order_item_id INT,
+    product_id VARCHAR(50) REFERENCES products(product_id),
+    seller_id VARCHAR(50) REFERENCES sellers(seller_id),
+    shipping_limit_date TIMESTAMP,
+    price NUMERIC(10, 2),
+    freight_value NUMERIC(10, 2)
+);
+
+CREATE TABLE order_payments (
+    order_id VARCHAR(50) REFERENCES orders(order_id),
+    payment_sequential INT,
+    payment_type VARCHAR(20),
+    payment_installments INT,
+    payment_value NUMERIC(10, 2)
+);
+
+CREATE TABLE order_reviews (
+    review_id VARCHAR(50),
+    order_id VARCHAR(50) REFERENCES orders(order_id),
+    review_score INT,
+    review_comment_title TEXT,
+    review_comment_message TEXT,
+    review_creation_date TIMESTAMP,
+    review_answer_timestamp TIMESTAMP
+);
+
